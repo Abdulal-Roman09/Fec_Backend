fb67ea3f1ed853112947a378fcb73b3f36a14bb1
Abdul Al Roman <mdroman45678910@gmail.com>
2025-09-26 09:32:05 +0600

    crate get by id and all acivements

--- controllers/achievmentController.js
+++ controllers/achievmentController.js
@@ -1,5 +1,6 @@
 import Achievement from "../models/Achivement.js";
 
+// Create Achievement
 export const createAchievement = async (req, res) => {
   try {
     const { clubId } = req.params;
@@ -8,10 +9,13 @@ export const createAchievement = async (req, res) => {
     if (!clubId || !title) {
       return res.status(400).json({ message: "clubId and title are required" });
     }
-    const eventExist = await Achievement.findOne({ clubId, title });
-    if (eventExist) {
-      return res.status(400).json({ message: "this Acivments is alrady exitst" });
+
+    // Check duplicate
+    const exist = await Achievement.findOne({ clubId, title });
+    if (exist) {
+      return res.status(400).json({ message: "This achievement already exists" });
     }
+
     const newAchievement = await Achievement.create({
       clubId,
       title,
@@ -24,6 +28,26 @@ export const createAchievement = async (req, res) => {
     res.status(201).json({
       message: "Achievement created successfully",
       data: newAchievement,
+    });
+  } catch (error) {
+    return res.status(500).json({ message: error.message });
+  }
+};
+
+// Get All Achievements / Club-specific
+export const allAchivements = async (req, res) => {
+  try {
+    const { clubId } = req.params;
+
+    const query = clubId ? { clubId } : {};
+
+    const achievements = await Achievement.find(query)
+      .populate("clubId", "name description")
+      .sort({ updatedAt: -1 });
+
+    res.status(200).json({
+      message: "Achievements fetched successfully",
+      data: achievements,
     });
   } catch (error) {
     return res.status(500).json({ message: error.message });
--- routes/achievementRoutes.js
+++ routes/achievementRoutes.js
@@ -1,9 +1,13 @@
 import express from "express";
-import { createAchievement } from "../controllers/achievmentController.js";
+import {
+  allAchivements,
+  createAchievement,
+} from "../controllers/achievmentController.js";
 
 const router = express.Router();
 
 router.post("/:clubId/add-achievements", createAchievement);
-
+router.get("/all-achievements", allAchivements);
+router.get("/clubs/:clubId/achievements", allAchivements);
 
 export default router;
