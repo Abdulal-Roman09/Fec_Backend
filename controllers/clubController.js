4f294af305f05492f8916d8f5db9b945d0db3a3d
Abdul Al Roman <mdroman45678910@gmail.com>
2025-10-05 17:04:48 +0600

    add serch and linmit and page in backend

--- controllers/clubController.js
+++ controllers/clubController.js
@@ -16,21 +16,44 @@ export const createClub = async (req, res) => {
 
 export const getAllclubs = async (req, res) => {
   try {
-    const { search } = req.query;
+    let { search, page, limit } = req.query;
+
+    page = parseInt(page) || 1;
+    limit = parseInt(limit) || 8;
+
     const query = {};
-    if (search) {
+
+    if (search && search.trim() !== "") {
       query.$or = [
         { clubName: { $regex: search, $options: "i" } },
         { clubSortName: { $regex: search, $options: "i" } },
         { clubCategory: { $regex: search, $options: "i" } },
       ];
     }
-    const clubs = await Club.find(query).sort({ createdAt: -1 });
-    res.status(200).json({ message: "Clubs fetched successfully", clubs });
+
+    const skip = (page - 1) * limit;
+
+    const total = await Club.countDocuments(query);
+    const totalPage = Math.ceil(total / limit);
+
+    const clubs = await Club.find(query)
+      .sort({ createdAt: -1 })
+      .skip(skip)
+      .limit(limit);
+
+    res.status(200).json({
+      message: "Clubs fetched successfully",
+      total,
+      page,
+      totalPage,
+      limit,
+      clubs,
+    });
   } catch (error) {
     return res.status(500).json({ message: error.message });
   }
 };
+
 export const getSingleClubs = async (req, res) => {
   try {
     const { id } = req.params;
