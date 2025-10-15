314e2a40afacd0b3b856a0d2a5e030d9be9c0f83
Abdul Al Roman <mdroman45678910@gmail.com>
2025-10-15 14:48:22 +0600

    add a new domain

--- src/app.js
+++ src/app.js
@@ -22,7 +22,7 @@ app.use(express.json());
 // CORS setup
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173", "https://fec-club-hub-client-qya2.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     allowedHeaders: [
       "Content-Type",
