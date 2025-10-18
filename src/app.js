7c59db8c72095d397b74eb1cbf01287ea9c14b7a
Abdul Al Roman <mdroman45678910@gmail.com>
2025-10-19 01:49:25 +0600

    fixed(/):remove the / from url

--- src/app.js
+++ src/app.js
@@ -24,7 +24,7 @@ app.use(
   cors({
     origin: [
       "http://localhost:5173",
-      "https://fec-club-hub-client.vercel.app/",
+      "https://fec-club-hub-client.vercel.app",
     ],
     methods: ["GET", "POST", "DELETE", "PUT", "OPTIONS"],
     allowedHeaders: [
